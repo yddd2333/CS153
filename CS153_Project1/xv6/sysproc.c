645cdbcd9ed7a497e24909cdeb05b767558442b5
Yang Deng <ydeng032@ucr.edu>
2017-12-31 23:03:58 -0800

    Add files via upload

---
+++ CS153_Project1/xv6/sysproc.c
@@ -0,0 +1,121 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  int s;
+  argint(0,&s);
+  exit(s);
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{ 
+  int *status;
+
+  argptr(0, (char**)&status,4);
+  return wait(status);
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int
+sys_waitpid(void)
+{
+  int pid, *status, options;
+  if(argint(0, &pid) < 0)
+  return -1;
+  argptr(1, (char**)&status,4);
+  if(argint(2, &options) < 0)
+  return -1;
+
+  return waitpid(pid,status,options);
+}
+
+int 
+sys_setpriority(void)
+{
+int pr;
+
+if(argint(0, &pr) < 0)
+return -1;
+
+return setpriority(pr);
+}
+
